4d158427ca6cb42dfc563ecf53d7f72ed1c9a191
Dimitrios Kombas <dimitrios.kombas@scania.com>
2023-10-09 10:00:01 +0200

    Completed price formatting and some UI customize

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image"
 import formatPrice from "../util/PriceFormat"
+import { ProductType } from "../types/AddCartType"
+import { metadata } from "../layout"
 
-export default function Product({ name, image, price }) {
+export default function Product({ name, image, price }: ProductType) {
     return (
         <div>
-            <Image src={image} alt={name} width={400} height={400} />
+            <Image src={image} alt={name} width={800} height={800} className="w-full h-96 object-cover" />
             <h1>{name}</h1>
-            {formatPrice(price)}
+            {price && formatPrice(price)}
         </div>
     )
 }
