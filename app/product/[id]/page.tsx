af35ab84826d907721a493c90cef9635bbc860b0
Dimitrios Kombas <dimitrios.kombas@scania.com>
2023-10-10 17:48:20 +0200

    Product Description Page

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import { SearchParamTypes } from "@/app/types/SearchParamTypes"
+import formatPrice from "@/app/util/PriceFormat"
+
+export default async function Product({ searchParams }: SearchParamTypes) {
+    return (
+        <div className="flex justify-between gap-24 p-12 text-gray-700">
+            <Image
+                src={searchParams.image}
+                alt={searchParams.name}
+                width={600}
+                height={600}
+            />
+            <div className="font-medium text-gray-700">
+                <h1 className="Name text-2xl py-2">{searchParams.name}</h1>
+                <p className="Description py-2">{searchParams.description}</p>
+                <p className="Metadata py-2">{searchParams.features}</p>
+                <div className="flex gap-2">
+                    <p className="font-bold text-teal-700">{searchParams.unit_amount && formatPrice(searchParams.unit_amount)} </p>
+                </div>
+                <button className="AddToCartButton my-12 text-white py-2 px-6 font-medium rounded-md bg-teal-700">Add to cart</button>
+            </div>
+        </div>
+    )
+} 
--- app/types/AddCartType.ts
+++
@@ -1,5 +0,0 @@
-export type ProductType = {
-    name : string
-    image: string
-    price: number | null
-}
